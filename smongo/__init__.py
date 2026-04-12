0d5efe2d8420b38cb6d59865efeb23cf2d834732
Fabian Valle <ranfys.valle@gmail.com>
2026-04-11 20:04:13 -0400

    1.2.4

--- smongo/__init__.py
+++ smongo/__init__.py
@@ -47,7 +47,7 @@ from .storage import DeleteResult, InsertResult, StreamingCursor, UpdateResult
 from .sync import SyncManager
 from .wire import WireServer
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 
 try:
     from ._smongo_core import __build_version__ as _native_version
